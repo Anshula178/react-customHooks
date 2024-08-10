dbd9f9e6d0f94c7d52ae9ed08e2ca34f9624bb3c
Anshula178 <anshula1708@gmail.com>
2024-08-10 23:21:06 +0530

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { useCounter } from "./hooks/useCounter";
 
 function App() {
   const { count, dispatch } = useCounter();
-
   return (
     <div className="flex items-center justify-center min-h-screen">
       <div className="bg-gray-50 w-1/2 space-y-3 text-center">
