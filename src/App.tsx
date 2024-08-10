d9457a9e0b3cd1d7b1bf15560adbbb8f772fe4ee
Anshula178 <anshula1708@gmail.com>
2024-08-10 23:46:43 +0530

    fix: size changes

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   const { count, dispatch } = useCounter();
   return (
     <div className="flex items-center justify-center min-h-screen">
-      <div className="bg-gray-50 w-1/2 space-y-3 text-center">
+      <div className="bg-gray-50 w-1/3 space-y-3 text-center">
         <p>{count}</p>
         <div className="flex items-center">
           <div className="flex gap-2 mx-auto">
